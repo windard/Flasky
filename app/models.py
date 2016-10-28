f0cebd63b9ff925be1b2b0b1f4a0da5b5872de40
windard <1106911190@qq.com>
2016-10-28 22:07:22 +0800

    init

---
+++ app/models.py
@@ -0,0 +1,14 @@
+# coding=utf-8
+
+class User(object):
+    """docstring for User"""
+    def __init__(self, arg):
+        super(User, self).__init__()
+        self.arg = arg
+
+class Role(object):
+    """docstring for Role"""
+    def __init__(self, arg):
+        super(Role, self).__init__()
+        self.arg = arg
+
