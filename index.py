bc5967c19e4480f911e5715f3bb37a2fc534106b
Yang Wenqiang <1106911190@qq.com>
2016-11-01 00:03:25 +0800

    2016-11-1

--- index.py
+++ index.py
@@ -1,8 +1,11 @@
 # coding=utf-8
 
-from flask import Flask,request,redirect,abort
+from flask import Flask, request, redirect, abort, make_response, render_template
+from flask_script import Manager
 
 app = Flask(__name__)
+# app.config['debug']=True
+# manager = Manager(app)
 
 @app.route('/')
 def index():
@@ -10,7 +13,9 @@ def index():
 
 @app.route('/user/<name>')
 def user(name):
-	return "<h2>Hello %s!</h2>"%(name)
+	return "It's Changed!"
+	# return render_template("user.html",name=name)
+	# return "<h2>Hello %s!</h2>"%(name)
 
 @app.route('/agent')
 def agent():
@@ -28,9 +33,28 @@ def test(num):
 	else:
 		abort(404)
 
+@app.route("/cookie")
+def cookie():
+	response = make_response("<h1>This document carries a cookies</h1>")
+	response.set_cookie("admin",'1')
+	return response
+
+# @app.route("/name/<name>")
+# def username(name):
+# 	return render_template("user.html",name=name)
+
+@app.route("/bad")
+def bad():
+	return "<h1>Bad Request</h1>",400
+	
+@app.route("/service")
+def service():
+	return "Service Unreliaziton",500
+
 @app.errorhandler(404)
 def page_not_found(error):
     return "<p>Sorry,No Found</p>", 404
 
 if __name__ == '__main__':
 	app.run(debug=True)
+	# manager.run()
